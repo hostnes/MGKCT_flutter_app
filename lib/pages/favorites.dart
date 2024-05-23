6b8c439cadd64e0b80d5e5dfc2d0c7d07cbbecb7
hostnes <lapanik2708@gmail.com>
2024-05-23 09:58:54 +0300

    dota obnova

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:work/components/app_bar.dart';
 import 'package:work/components/bottom_bar.dart';
 
-class FavorietsPage extends StatelessWidget {
+class FavorietsPage extends StatefulWidget {
   const FavorietsPage({super.key});
 
   @override
+  State<FavorietsPage> createState() => _FavorietsPageState();
+}
+
+class _FavorietsPageState extends State<FavorietsPage> {
+  List _boxGroups = [];
+  List _boxTeachers = [];
+  List _boxElements = [];
+  var box = Hive.box('userInfo');
+
+  @override
+  void initState() {
+    _boxTeachers = box.get('teachers', defaultValue: []);
+    _boxGroups = box.get('groups', defaultValue: []);
+    _boxElements = _boxTeachers + _boxGroups;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: SearchAppBar(title: 'Favoriets'),
       bottomNavigationBar: BottomBar(),
+      body: Container(
+        margin: const EdgeInsets.all(7),
+        child: ListView.builder(
+          itemCount: _boxElements.length,
+          itemBuilder: (context, index) {
+            print(_boxElements[index].runtimeType);
+
+            return Text(_boxElements[index].toString());
+          },
+        ),
+      ),
     );
   }
 }
